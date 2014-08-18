138ed2760cf2b5f2d65118617ecfc61d3234a641
Hassan Zaki <hassan.zaki@badrit.com>
2014-08-18 22:52:06 +0300

    * fix reports calculations based on new requirements

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,12 @@
 class HomeController < ApplicationController
 
   def index
-    @top_paying_customers = Customer.with_top_payments.limit(10)
-    @top_ordering_customers = Customer.with_top_orders.limit(10)
-    @top_remaining_customers = Customer.with_top_remaining.limit(10)
+    @top_paying_customers = Customer.top_payments.limit(10)
+    @top_ordering_customers = Customer.top_orders.limit(10)
+    @top_remaining_customers = Customer.top_remaining.limit(10)
 
-    @low_paying_customers = Customer.with_low_payments.limit(10)
-    @low_ordering_customers = Customer.with_low_orders.limit(10)
-    @low_remaining_customers = Customer.with_low_remaining.limit(10)
-
-
+    @low_paying_customers = Customer.low_payments.limit(10)
+    @low_ordering_customers = Customer.low_orders.limit(10)
+    @low_remaining_customers = Customer.low_remaining.limit(10)
   end
 end
--- app/models/concerns/customer_supplier.rb
+++ app/models/concerns/customer_supplier.rb
@@ -5,37 +5,76 @@ module CustomerSupplier
     has_many :source_transactions, as: :source, class_name: 'Transaction', after_add: :update_source
     has_many :destination_transactions, as: :source, class_name: 'Transaction', after_add: :update_destination
 
-    before_validation :set_user_name, if: ->(u){u.username.blank?}
+    before_validation :set_user_name, if: ->(u){ u.username.blank? }
 
       QUERY_SELECT_FIELDS = 'users.id, users.email, users.phone, users.first_name, users.last_name'
       QUERY_ORDER_FIELD = ' SUM(orders.total) as total_amount'
       QUERY_PAYMENT_FIELD = ' SUM(payments.amount) as paid_amount'
       QUERY_REMAINING_FIELD = ' SUM(orders.total - payments.amount) as remaining'
-      
-      scope :with_payments_and_orders, ->(){ joins('LEFT OUTER JOIN orders ON orders.customer_id = users.id ')
-                                               .joins('LEFT OUTER JOIN payments ON payments.user_id = users.id')
-                                               .select("#{QUERY_SELECT_FIELDS}, #{QUERY_ORDER_FIELD}, #{QUERY_PAYMENT_FIELD}, #{QUERY_REMAINING_FIELD}")
-                                               .group('users.id') 
-                                            }
-      
-      scope :with_top_remaining, ->(){ with_payments_and_orders.order('remaining DESC') }
-      scope :with_low_remaining, ->(){ with_payments_and_orders.order('remaining') }
 
-      scope :with_orders, ->(){ joins('LEFT OUTER JOIN orders ON orders.customer_id = users.id ')
-                                  .select("#{QUERY_SELECT_FIELDS}, #{QUERY_ORDER_FIELD}")
-                                  .group('users.id')
+      scope :top_remaining, ->(){ 
+                                  select('id, first_name, last_name, SUM(total_amount - total_paid) as total_remaining')
+                                  .group('id')
+                                  .order('total_remaining DESC')
                                 }
 
-      scope :with_payments, ->(){ joins('LEFT OUTER JOIN payments ON payments.user_id = users.id ')
-                                    .select("#{QUERY_SELECT_FIELDS}, #{QUERY_PAYMENT_FIELD}")
-                                    .group('users.id') 
+      scope :low_remaining, ->(){ 
+                                  select('id, first_name, last_name, SUM(total_amount - total_paid) as total_remaining')
+                                  .group('id')
+                                  .order('total_remaining')
                                 }
 
-      scope :with_top_orders, ->(){ with_orders.order('total_amount DESC') }
-      scope :with_top_payments, ->(){ with_payments.order('paid_amount DESC') }
+      scope :top_payments,  ->(){ 
+                                select('id, first_name, last_name, SUM(total_paid) as total_payment')
+                                .group('id')
+                                .order('total_payment DESC')
+                              }
 
-      scope :with_low_orders, ->(){ with_orders.order('total_amount') }
-      scope :with_low_payments, ->(){ with_payments.order('paid_amount') }
+      scope :low_payments,  ->(){ 
+                                select('id, first_name, last_name, SUM(total_paid) as total_payment')
+                                .group('id')
+                                .order('total_payment')
+                              }
+
+      scope :top_orders,  ->(){
+                                joins('LEFT OUTER JOIN orders ON orders.customer_id = users.id ') 
+                                .select('users.id, users.first_name, users.last_name, SUM(orders.total) as total_amount')
+                                .group('users.id')
+                                .order('total_amount DESC')
+                              }
+
+      scope :low_orders,  ->(){ 
+                                joins('LEFT OUTER JOIN orders ON orders.customer_id = users.id ') 
+                                .select('users.id, users.first_name, users.last_name, SUM(orders.total) as total_amount')
+                                .group('users.id')
+                                .order('total_amount')
+                              }
+
+      
+      # scope :with_payments_and_orders, ->(){ joins('LEFT OUTER JOIN orders ON orders.customer_id = users.id ')
+      #                                          .joins('LEFT OUTER JOIN payments ON payments.user_id = users.id')
+      #                                          .select("#{QUERY_SELECT_FIELDS}, #{QUERY_ORDER_FIELD}, #{QUERY_PAYMENT_FIELD}, #{QUERY_REMAINING_FIELD}")
+      #                                          .group('users.id') 
+      #                                       }
+      
+      # scope :with_top_remaining, ->(){ with_payments_and_orders.order('remaining DESC') }
+      # scope :with_low_remaining, ->(){ with_payments_and_orders.order('remaining') }
+
+      # scope :with_orders, ->(){ joins('LEFT OUTER JOIN orders ON orders.customer_id = users.id ')
+      #                             .select("#{QUERY_SELECT_FIELDS}, #{QUERY_ORDER_FIELD}")
+      #                             .group('users.id')
+      #                           }
+
+      # scope :with_payments, ->(){ joins('LEFT OUTER JOIN payments ON payments.user_id = users.id ')
+      #                               .select("#{QUERY_SELECT_FIELDS}, #{QUERY_PAYMENT_FIELD}")
+      #                               .group('users.id') 
+      #                           }
+
+      # scope :with_top_orders, ->(){ with_orders.order('total_amount DESC') }
+      # scope :with_top_payments, ->(){ with_payments.order('paid_amount DESC') }
+
+      # scope :with_low_orders, ->(){ with_orders.order('total_amount') }
+      # scope :with_low_payments, ->(){ with_payments.order('paid_amount') }
     
     def password_required?
       false
